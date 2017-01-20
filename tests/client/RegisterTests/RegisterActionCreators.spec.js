1545469a8904ec5fcc6d55de364723df034a2f4b
mlampedx <mlampesd@gmail.com>
2017-01-20 11:54:03 -0800

    Added tests for Register component, action creators, and reducers

--- tests/client/RegisterTests/RegisterActionCreators.spec.js
+++ tests/client/RegisterTests/RegisterActionCreators.spec.js
@@ -1,15 +1,18 @@
 import { expect } from 'chai';
 import nock from 'nock';
 import mockStore from './../fixtures/mockStore';
-import { registerActionFixtures, mockRegisterActionPayload } from './../fixtures/expectedRegisterActions';
+import {
+  registerActionSuccessFixtures,
+  registerActionFailureFixtures,
+  registerMockActionPayload,
+} from './../fixtures/RegisterFixtures/registerActionFixtures';
 // import * as actions from '../../../../src/client/actions/index';
 // import * as types from '../../../../src/client/actions/ActionTypes';
 
-describe('Register Action Creators', () => {
-  afterEach(() => {
+describe('Action Creator: registerUser', () => {
+  after(() => {
     nock.cleanAll();
   });
-
 
   it('dispatches REGISTER_USER_SUCCESS action type when post request successfully completes', () => {
     nock('http://example.com')
@@ -22,9 +25,26 @@ describe('Register Action Creators', () => {
       })
       .reply('201', { status: 'success' });
 
-    return mockStore.dispatch(actions.registerUser(mockRegisterActionPayload))
+    return mockStore.dispatch(actions.registerUser(registerMockActionPayload))
       .then(() => {
-        expect(mockStore.getActions()).to.be.equal.to(registerActionFixtures);
+        expect(mockStore.getActions()).to.be.equal.to(registerActionSuccessFixtures);
+      });
+  });
+
+  it('dispatches REGISTER_USER_FAILURE action type when post request fails due to pre-existing resource in db', () => {
+    nock('http://example.com')
+      .post('/register', {
+        name: 'John Doe',
+        email: 'jdoe@example.com',
+        username: 'jaydee',
+        password: 'password123',
+        passwordconfirmation: 'password123',
+      })
+      .reply('409', { status: 'failure' });
+
+    return mockStore.dispatch(actions.registerUser(registerMockActionPayload))
+      .then(() => {
+        expect(mockStore.getActions()).to.be.equal.to(registerActionFailureFixtures);
       });
   });
 });
