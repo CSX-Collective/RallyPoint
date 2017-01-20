509fa48b716cf808996d3b29f618fd6a4c58037f
mlampedx <mlampesd@gmail.com>
2017-01-19 19:43:33 -0800

    Configured webpack for development environment

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const PATHS = {
+  app: './src/client/index.jsx',
+  html: './src/client/index.html',
+  dist: path.join(__dirname, 'dist'),
+};
+
+module.exports = {
+  entry: {
+    javascript: PATHS.app,
+    html: PATHS.html,
+  },
+  output: {
+    path: PATHS.dist,
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    devServer: {
+      proxy: {
+        '**': {
+          target: 'http://localhost:3000',
+          secure: false,
+        },
+      },
+    },
+    contentBase: PATHS.dist,
+  },
+  eslint: {
+    emitWarning: true,
+  },
+  module: {
+    loaders: [{
+      test: /\.html$/,
+      loader: 'file?name=[name].[ext]',
+    }, {
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      loaders: ['babel-loader'],
+    }, {
+      test: /\.(css|scss)$/,
+      loaders: ['style', 'css', 'sass'],
+    }],
+  },
+  externals: {
+    cheerio: 'window',
+    'react/lib/ExecutionEnvironment': true,
+    'react/lib/ReactContext': true,
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx', 'html', 'css', 'scss'],
+  },
+};
