98922f50051706351f05d55314b1011aaf8cfe28
Michael Laythe <mrlaythe24@aol.com>
2017-01-21 12:55:55 -0800

    make email unique for indexing later

--- server/users/userModel.js
+++ server/users/userModel.js
@@ -3,11 +3,13 @@ const logger = require('../logs/logger');
 
 db.query(`create table if not exists "users" (
   _id serial primary key,
-  email varchar(256),
+  email varchar(256) unique,
   password varchar(256),
   first_name varchar(256),
   last_name varchar(256),
   dob date
-)`, (err) => logger.error(err));
+)`, (err) => {
+  if (err) logger.error(err);
+});
 
 module.exports = db;
