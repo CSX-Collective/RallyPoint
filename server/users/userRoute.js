ddc14a11c2d68f063c570a1cc52874db98451620
Michael Laythe <mrlaythe24@aol.com>
2017-01-21 12:56:33 -0800

    add POST /users object schema

--- server/users/userRoute.js
+++ server/users/userRoute.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const userCtrl = require('./userCtrl');
+const Joi = require('joi');
+const validate = require('../utils/validate');
 
 const userRoute = module.exports = express.Router();
 
+const createUserSchema = {
+  email: Joi.string().email().required(),
+  password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/).required(),
+  first_name: Joi.string().alphanum().min(3).max(30).required(),
+  last_name: Joi.string().alphanum().min(3).max(30).required(),
+  dob: Joi.date().required(),
+};
+
 userRoute.get('/', userCtrl.getUsers);
-userRoute.post('/');
+userRoute.post('/', validate(createUserSchema), userCtrl.createUser);
