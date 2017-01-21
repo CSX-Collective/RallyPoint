1b734e31be084a8abac5eeb95a2d3cd32e316092
Michael Laythe <mrlaythe24@aol.com>
2017-01-21 13:46:38 -0800

    add morgan for http logging

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const path = require('path');
+const morgan = require('morgan');
 const logger = require('./logs/logger');
 const userRoute = require('./users/userRoute');
 
@@ -11,6 +12,10 @@ const PORT = 8080;
 
 app.use(cookieParser());
 app.use(bodyParser.json());
+
+if (process.env.NODE_ENV === 'test') {
+  app.use(morgan('dev'));
+}
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../src/client/index.html'));
