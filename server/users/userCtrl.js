d33cb9fe1cf411864a8def9477e162bb51230271
Michael Laythe <mrlaythe24@aol.com>
2017-01-21 12:55:26 -0800

    add createUser method to handle POST requests

--- server/users/userCtrl.js
+++ server/users/userCtrl.js
@@ -2,6 +2,17 @@ const db = require('./userModel');
 const logger = require('../logs/logger');
 
 const userCtrl = {
+  
+  createUser: (req, res, next) => {
+    const user = req.body;
+ 
+    db.query(`insert into users (email, password, first_name, last_name, dob) values ($1, $2, $3, $4, $5)`, [user.email, user.password, user.first_name, user.last_name, user.dob], (err, user) => {
+      if (err) logger.error(err);
+
+      return res.status(201).send(user);
+    });
+  },
+
   getUsers: (req, res, next) => {
     db.query('select * from users', (err, users) => {
       if (err) logger.error(err);
