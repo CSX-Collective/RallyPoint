30629a1df52be139567ef658bfd6f26e3ea19efd
Michael Laythe <mrlaythe24@aol.com>
2017-01-22 23:28:35 -0800

    add email to session upon successful signup || login & add helper methods for encrypting and decrypting password

--- server/users/userCtrl.js
+++ server/users/userCtrl.js
@@ -1,14 +1,18 @@
 const db = require('./userModel');
 const logger = require('../logs/logger');
+const bcrypt = require('bcrypt');
+const SALT_FACTOR = 10;
 
 const userCtrl = {
 
   createUser: (req, res, next) => {
     const user = req.body;
+    const password = encryptPassword(user.password);
  
-    db.query(`insert into users (email, password, first_name, last_name, dob) values ($1, $2, $3, $4, $5)`, [user.email, user.password, user.first_name, user.last_name, user.dob], (err) => {
+    db.query(`insert into users (email, password, first_name, last_name, dob) values ($1, $2, $3, $4, $5)`, [user.email, password, user.first_name, user.last_name, user.dob], (err) => {
       if (err) logger.error(err);
 
+      req.session.key = user.email;
       return res.status(201).end();
     });
   },
@@ -22,7 +26,7 @@ const userCtrl = {
   },
 
   getUserById: (req, res, next) => {
-    db.query(`select * from users where _id= ${req.params.user_id}`, (err, user) => {
+    db.query(`select * from users where _id= ${req.params.user_id} limit 1`, (err, user) => {
       if (err) logger.error(err);
 
       return res.status(200).send(user.rows[0]);
@@ -34,6 +38,23 @@ const userCtrl = {
       if (err) logger.error(err);
 
       return res.status(200).send(users.rows);
+    });
+  },
+
+  loginUser: (req, res, next) => {
+    db.query(`select * from users where email= '${req.body.email}' limit 1`, (err, user) => {
+      if (err) logger.error(err);
+
+      const password = user.rows[0].password;
+
+      if (decryptPassword(req.body.password, password)) {
+        req.session.key = req.body.email;
+        // eventually redirect to home
+        return res.status(201).end();
+      } else {
+        // passwords don't match
+        return res.status(401).end();
+      }
     });
   },
 
@@ -56,6 +77,16 @@ const userCtrl = {
       return res.status(204).end();
     });
   },
+};
+
+const encryptPassword = (password) => {
+  const salt = bcrypt.genSaltSync(SALT_FACTOR);
+  const hash = bcrypt.hashSync(password, salt);
+  return hash;
+};
+
+const decryptPassword = (plainPassword, hashedPassword) => {
+  return bcrypt.compareSync(plainPassword, hashedPassword);
 };
 
 module.exports = userCtrl;
