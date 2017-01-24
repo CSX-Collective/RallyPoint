6a6fb1d1b0ec55169959788a4a088479979a051b
Michael Laythe <mrlaythe24@aol.com>
2017-01-23 22:25:43 -0800

    add not null property to each  column

--- server/users/userModel.js
+++ server/users/userModel.js
@@ -3,11 +3,11 @@ const logger = require('../logs/logger');
 
 db.query(`create table if not exists "users" (
   _id serial primary key,
-  email varchar(256) unique,
-  password varchar(256),
-  first_name varchar(256),
-  last_name varchar(256),
-  dob date
+  email varchar(256) unique not null,
+  password varchar(256) not null,
+  first_name varchar(256) not null,
+  last_name varchar(256) not null,
+  dob date not null
 )`, (err) => {
   if (err) logger.error(err);
 });
