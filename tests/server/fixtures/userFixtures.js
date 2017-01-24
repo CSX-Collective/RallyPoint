3d0ed307bce9b9bb42b45d8d6258863f0c3b4ae5
Michael Laythe <mrlaythe24@aol.com>
2017-01-24 00:06:38 -0800

    password must contain one digit, one lowercase, one uppercase, and 8 characters minimum

--- tests/server/fixtures/userFixtures.js
+++ tests/server/fixtures/userFixtures.js
@@ -3,7 +3,7 @@ const { encryptPassword } = require('../../../server/utils/validate');
 module.exports = {
   _id: 1,
   email: 'test@xyz.io',
-  password: encryptPassword('shoes2231'),
+  password: encryptPassword('Shoes2231'),
   first_name: 'Tony',
   last_name: 'Tiger',
   dob: '2017-10-1',
