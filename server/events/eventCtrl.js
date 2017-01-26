560c07f51cd012b56d17fded441a9554639b371b
Edward Park <ed.sh.park@gmail.com>
2017-01-25 20:09:54 -0800

    added shell for eventCtrl

---
+++ server/events/eventCtrl.js
@@ -0,0 +1,10 @@
+const db = require('./eventModel');
+const logger = require('../logs/logger');
+
+const eventCtrl = {
+  createEvent: (req, res) => { },
+  deleteEvent: (req, res) => { },
+  updateEvent: (req, res) => { },
+  getEventById: (req, res) => { },
+  getEvents: (req, res) => { },
+}
