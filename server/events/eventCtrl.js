7af7983d0e085503c31303ebedbb3975f0a74a3f
Edward Park <ed.sh.park@gmail.com>
2017-01-26 21:12:21 -0800

    added createEvent method

--- server/events/eventCtrl.js
+++ server/events/eventCtrl.js
@@ -2,7 +2,18 @@ const db = require('./eventModel');
 const logger = require('../logs/logger');
 
 const eventCtrl = {
-  createEvent: (req, res) => { },
+  createEvent: (req, res) => {
+    const { title, desc, start, end, location, min_age, category } = req.body;
+
+    db.query(`INSERT INTO events (events, desc, start, end location, min_age, category) VALUES (${title}, ${desc}, ${start}, ${end}, ${location}, ${min_age}, ${category})`, (err) => {
+      if (err) {
+        logger.error(err);
+        return res.status(400).end();
+      }
+
+      return res.status(201).end();
+    });
+  },
   deleteEvent: (req, res) => { },
   updateEvent: (req, res) => { },
   getEventById: (req, res) => { },
