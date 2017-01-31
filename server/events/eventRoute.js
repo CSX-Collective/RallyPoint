5f62b675d9923f09d086522b60e91187ed17092d
Micky Shaked <micky.shaked@gmail.com>
2017-01-31 14:05:28 -0800

    added comments routes to eventRoute.js

--- server/events/eventRoute.js
+++ server/events/eventRoute.js
@@ -2,7 +2,7 @@ const express = require('express');
 const eventCtrl = require('./eventCtrl');
 const Joi = require('joi');
 const { validateSchema } = require('../utils/validate');
-
+const commentRoute = require('../comments/commentRoute')
 const eventRoute = module.exports = express.Router();
 
 eventRoute.get('?location');
@@ -11,4 +11,6 @@ eventRoute.post('?location');
 eventRoute.get('/:event_id');
 eventRoute.delete('/:event_id');
 eventRoute.patch('/:event_id');
+
+eventRoute.use('/:event_id/comments', commentRoute)
 
